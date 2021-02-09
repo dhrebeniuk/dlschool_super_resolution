b0b269aaa45b6c6e485a25e68fbbde202ac6462e
Dmytro Hrebeniuk <dmytrohrebeniuk@gmail.com>
2021-02-09 19:32:13 +0200

    Quiet mode for extract data from zip

--- bin/download_dataset.sh
+++ bin/download_dataset.sh
@@ -2,4 +2,4 @@
 wget -q https://uofi.box.com/shared/static/65upg43jjd0a4cwsiqgl6o6ixube6klm.zip -O urban100.zip
 
 mkdir urban100
-unzip urban100.zip -d  urban100
+unzip -qq urban100.zip -d  urban100
